98f6cbdceeb2d3946b00091763d66cec92cb9074
todynyuk <todynyuk@solvd.com>
2023-02-01 19:23:00 +0200

    add python web-tests for demoqa.com

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -6,3 +6,13 @@ class MainPageLocators:
     SEARCH_BUTTON = (By.CSS_SELECTOR, "button[class*='button_color_green']")
     GOODS_TITLE_TEXTS = (By.XPATH, "//span[@class='goods-tile__title']")
     NOT_FOUND_TEXT = (By.XPATH, "//span[@class='ng-star-inserted']")
+
+
+class DevicePageLocators:
+    SHORT_CHARACTERISTICS_TITLE = (By.XPATH, "//h1[@class='product__title']")
+    PRODUCT_PRICE = (By.XPATH, "//p[contains(@class,'product-prices__big')]")
+    SHORT_CHARACTERISTIC = (By.XPATH, "//p[@class='product-about__brief ng-star-inserted']")
+    ALL_CHARACTERISTIC = (By.XPATH,
+                          "//h3[@class='product-tabs__heading']//span[contains(@class,'heading_color_gray')]")
+
+
