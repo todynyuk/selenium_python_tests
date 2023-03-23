a8b04c5688051aa1b723e253c5a1aae620662294
todynyuk <todynyuk@solvd.com>
2023-03-23 14:04:14 +0200

    change conftest and add base_test

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def driver():
     zebrunner_selenium_grid = os.getenv("ZEBRUNNER_SELENIUM_GRID", "http://127.0.0.1:4444")
     options = webdriver.ChromeOptions()
     options.platform_name = "linux"
-    options.browser_version = "111.0"
+    options.browser_version = "110.0"
     options.set_capability("enableVideo", "true")
     driver = webdriver.Remote(
         command_executor=zebrunner_selenium_grid,
